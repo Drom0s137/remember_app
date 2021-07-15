a8ad55cc329b840e5154bda1e0b9bd49bf9559cb
Eddy <eddy.che.su@gmail.com>
2021-07-14 23:50:03 -0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:line_icons/line_icons.dart';
+import 'package:flutter_clean_calendar/flutter_clean_calendar.dart';
+import 'calendar.dart';
+
+void main() => runApp(MaterialApp(
+    builder: (context, child) {
+      return Directionality(textDirection: TextDirection.ltr, child: child!);
+    },
+    title: 'Remember?',
+    theme: ThemeData(
+      primaryColor: Colors.grey[800],
+    ),
+    home: MyHomePage()));
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _NavBar createState() => _NavBar();
+}
+
+class _NavBar extends State<MyHomePage> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.w600);
+  static List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Reminders',
+      style: optionStyle,
+    ),
+    CalendarScreen(),
+    Text(
+      'Timeline',
+      style: optionStyle,
+    ),
+    Text(
+      'Focus',
+      style: optionStyle,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 20,
+        title: const Text('Remember?'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 20,
+              color: Colors.black.withOpacity(.1),
+            )
+          ],
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
+            child: GNav(
+              rippleColor: Colors.grey[300]!,
+              hoverColor: Colors.grey[100]!,
+              gap: 8,
+              activeColor: Colors.black,
+              iconSize: 24,
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+              duration: Duration(milliseconds: 400),
+              tabBackgroundColor: Colors.grey[100]!,
+              color: Colors.black,
+              tabs: [
+                GButton(
+                  icon: LineIcons.list,
+                  text: 'Reminders',
+                ),
+                GButton(
+                  icon: LineIcons.calendar,
+                  text: 'Calendar',
+                ),
+                GButton(
+                  icon: LineIcons.play,
+                  text: 'Timeline',
+                ),
+                GButton(
+                  icon: LineIcons.hourglass,
+                  text: 'Focus',
+                ),
+              ],
+              selectedIndex: _selectedIndex,
+              onTabChange: (index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
