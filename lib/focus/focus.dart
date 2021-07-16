a616f16d19193c7736a61d5d220239dbcf8a1cde
Willie Pai <PaisWillie@gmail.com>
2021-07-15 23:29:55 -0400

    Fixed focus.dart

--- lib/focus/focus.dart
+++ lib/focus/focus.dart
@@ -2,29 +2,22 @@ import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 import 'package:flutter/material.dart';
 
 class FocusScreen extends StatefulWidget {
-  FocusScreen({Key? key}) : super(key: key);
-
   @override
   _FocusScreenState createState() => _FocusScreenState();
 }
 
-class _FocusScreenState extends State<Focus> {
+class _FocusScreenState extends State<FocusScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            CircularCountDownTimer(
-              width: MediaQuery.of(context).size.width / 2,
-              height: MediaQuery.of(context).size.height / 2,
-              duration: 10,
-              fillColor: Colors.blue,
-              ringColor: Colors.grey,
-            )
-          ],
-        ),
-      ),
+    // return Scaffold(
+    //   body: Text('Hi'),
+    // );
+    return CircularCountDownTimer(
+      width: MediaQuery.of(context).size.width / 2,
+      height: MediaQuery.of(context).size.height / 2,
+      duration: 10,
+      fillColor: Colors.blue,
+      ringColor: Colors.grey,
     );
   }
 }
