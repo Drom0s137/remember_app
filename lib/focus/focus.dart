1d8c796e5913254f5720432c47ce31a247d3830b
Willie Pai <PaisWillie@gmail.com>
2021-07-15 21:34:35 -0400

    Update focus.dart

--- lib/focus/focus.dart
+++ lib/focus/focus.dart
@@ -12,16 +12,18 @@ class _FocusScreenState extends State<Focus> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          CircularCountDownTimer(
-            width: MediaQuery.of(context).size.width / 2,
-            height: MediaQuery.of(context).size.height / 2,
-            duration: 10,
-            fillColor: Colors.blue,
-            ringColor: Colors.grey,
-          )
-        ],
+      body: SafeArea(
+        child: Column(
+          children: [
+            CircularCountDownTimer(
+              width: MediaQuery.of(context).size.width / 2,
+              height: MediaQuery.of(context).size.height / 2,
+              duration: 10,
+              fillColor: Colors.blue,
+              ringColor: Colors.grey,
+            )
+          ],
+        ),
       ),
     );
   }
